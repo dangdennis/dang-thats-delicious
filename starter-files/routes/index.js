8ab4dc56eaf4cdf051718bdf2ff03e703457ccdc
Dennis Dang <dangggdennis@gmail.com>
2017-07-12 18:15:16 -0700

    version 4

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+	const den = { name: 'dennis', age: 500 };
+  // res.send('why doesnt this work right');
+  res.json(den);
 });
+
+router.get('/reverse/:name', (req,res) => {
+	const reverse = [...req.params.name].reverse().join('');
+	console.log(reverse);
+	res.send(reverse);
+})
 
 module.exports = router;
